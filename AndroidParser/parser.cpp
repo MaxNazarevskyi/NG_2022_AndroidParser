47ea84a3a1b3f0fbbb6c6bda1ff3f09fc2b7ed85
Maksim Nazarevskyi <ithefirstly@gmail.com>
2022-07-12 23:49:36 +0300

    Try to do search better(bad)

--- AndroidParser/parser.cpp
+++ AndroidParser/parser.cpp
@@ -10,7 +10,6 @@ Parser::Parser(QWidget *parent)
     ui->setupUi(this);
     connect (ui->b_Search, &QPushButton::clicked, this, &Parser::addedStorage);
     connect (ui->l_Proc, &QLineEdit::textChanged, this, &Parser::addedStorage);
-    //connect(Watcher, SIGNAL(directoryChanged(QString)), this, SLOT(showModifiedDirectory(QString)));
 }
 
 Parser::~Parser()
@@ -31,8 +30,10 @@ void Parser::addedStorage()
     Proc.waitForFinished();
     QString output (Proc.readAllStandardOutput());
     QStringList outputList = output.split("\n");
-        if(outputList.at(44).contains("Hardware")){
-            ui->l_Proc->setText(outputList.at(44).right(outputList.at(44).indexOf(":")));
+    foreach (QString proc, outputList)
+        if(proc.contains("Hardware")){
+            ui->l_Proc->setText(proc);
+            break;
         }
         else
             ui->l_Proc->setText("Unknown");
